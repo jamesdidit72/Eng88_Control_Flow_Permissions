ab99850a6ed938bfea0d0d13d69284f57a0d0e65
jamesdidit72 <jamesjbarton72@gmail.com>
2021-05-20 16:57:52 +0100

    progress on drivers license task

--- control_flow_permissions.py
+++ control_flow_permissions.py
@@ -1,2 +1,18 @@
-age = 19
-driver_license = True
+leave = True
+while leave:
+    user_prompt = True
+    continue_ = input('Do you want to continue? Y/N  ')
+    if continue_.upper == 'Y':
+        while user_prompt:
+            age = input('Please enter your age:  ')
+            if age.isdigit():
+                user_prompt = False
+            else:
+                print('Please enter a digit')
+        driver_license_input = input('Do you have a drivers license? Y/N ')
+        if driver_license_input.upper == 'Y':
+            drivers_license = True
+        else:
+            drivers_license = False
+    else:
+        leave = False
